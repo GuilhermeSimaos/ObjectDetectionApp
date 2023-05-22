0b66e0d7d32f588fb1b388d615a05d1af2e668a5
Guilherme Simao <guilhermesimaoGH@outlook.com>
2023-05-22 17:48:59 -0300

    Migrated code to async

--- src/App.js
+++ src/App.js
@@ -1,49 +1,121 @@
-import React, {useState} from 'react';
+import React, { useRef, useEffect, useState } from 'react';
 import axios from 'axios';
 
-function App(){
-    const [data, setData] = useState();
-    const [focusedButton, setFocusedButton] = useState('fetch');
+function App() {
+  const videoRef = useRef();
+  const photoRef = useRef();
+  const [hasPhoto, setHasPhoto] = useState(false);
+  const [cameraSettings, setCameraSettings] = useState(false);
+  const [hideButton, setHideButton] = useState(false);
 
-    async function fetchData() {
-        try{
-            let response = await axios.get('https://objdetectionserver-production.up.railway.app/api/data');
-            setData(response.data);
-            setFocusedButton('clear')
-        }catch (error){
-            console.error(error);
-        }
+  // Get video input from the device
+  const getVideo = () => {
+    navigator.mediaDevices.getUserMedia({ video: {} })
+      .then(stream => {
+        setCameraSettings(stream.getVideoTracks()[0].getSettings());
+        let video = videoRef.current;
+        video.srcObject = stream;
+        video.play();
+      }).catch(err => {
+        if (err.name === 'NotAllowedError')
+          console.log('Permission denied for camera access.');
+        else
+          console.log(err);
+      });
+  };
+
+// Take photo from the current video stream
+const takePhoto = async () => {
+    // Define canvas with a high resolution
+    let canvas = document.createElement('canvas');
+    canvas.width = cameraSettings.width;
+    canvas.height = cameraSettings.height;
+  
+    // Draw the photo on the canvas
+    let ctx = canvas.getContext('2d');
+    ctx.drawImage(videoRef.current, 0, 0, canvas.width, canvas.height);
+  
+    // Converts to Blob and send to backend
+    canvas.toBlob(async (blob) => {
+      // Add in a formData
+      const formData = new FormData();
+      formData.append('image', blob, 'my-photo.jpg');
+  
+      // Use axios to post to endpoint
+      try {
+        await axios.post('https://objdetectionserver-production.up.railway.app/post-photo', formData);
+        console.log('Image sent successfully!');
+      } catch (error) {
+        console.log('Error sending image!!!', error);
+      }
+  
+      // Wait for 5 seconds
+      await new Promise((resolve) => setTimeout(resolve, 4000));
+  
+      // Get processed photo
+      await getProcessedPhoto();
+    });
+  };
+
+  // Get processed photo from backend
+  const getProcessedPhoto = async () => {
+    // Points to the endpoint specifying the response type
+    try {
+      const response = await axios.get('https://objdetectionserver-production.up.railway.app/get-processed-photo', { responseType: 'blob' });
+      // Converts blob to file then read it
+      const reader = new FileReader();
+      reader.onload = () => {
+        const img = new Image();
+        img.onload = () => {
+          // Draw image on canvas element
+          const canvas = photoRef.current;
+          canvas.width = img.width;
+          canvas.height = img.height;
+          const context = canvas.getContext('2d');
+          context.drawImage(img, 0, 0);
+          setHasPhoto(true);
+        };
+        img.src = reader.result;
+      };
+      reader.readAsDataURL(response.data);
+      setHideButton(true);
+    } catch (error) {
+      console.log(error);
     }
+  };
 
-    function clearData() {
-        setData(null);
-        setFocusedButton('fetch');
+  // Clear canvas from the taken photo
+  const closePhoto = async () => {
+    setHasPhoto(false);
+    setHideButton(false);
+    // Delete temporary files from backend
+    try {
+      const response = await axios.delete('https://objdetectionserver-production.up.railway.app/delete-files');
+      console.log(response);
+    } catch (error) {
+      console.log(error);
     }
+  };
 
-    // HTML components
-    return (
-        <div className="App">
-            <button 
-            className="fetch"
-            onClick={fetchData}
-            style={{display: focusedButton.match('fetch')? 'initial':'none'}}
-            >Fetch Data</button>
+  useEffect(() => {
+    getVideo();
+  }, []);
 
-            <div style={{
-                paddingTop:"7em",
-                color:"white",
-                fontWeight:"bolder",
-                fontSize:"3em"}}>
-                {data ? <p>{data.message}</p> : <p>Loading...</p>}
-            </div>
-
-            <button 
-            className="clear"
-            onClick={clearData}
-            style={{display: focusedButton.match('clear')? 'initial':'none'}}
-            >Clear Data</button>
-        </div>
-    );
+  // HTML components
+  return (
+    <div className="App">
+      <div className="camera">
+        <h1>Webcam</h1>
+        <video ref={videoRef}></video>
+      </div>
+      <button hidden={hideButton} onClick={takePhoto}>Tirar Foto</button>
+      <div className={'result' + (hasPhoto ? 'hasPhoto' : '')}>
+        <h1>Foto Processada</h1>
+        <canvas ref={photoRef}></canvas>
+        <button onClick={closePhoto}>Apagar Foto</button>
+      </div>
+    </div>
+  );
 }
 
 export default App;
